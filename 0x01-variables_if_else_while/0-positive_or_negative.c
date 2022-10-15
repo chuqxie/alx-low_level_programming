ce4e84a7349cf0571d581fa1aae18e764dcf445e
chuqxie <ekechukwudi63@gmail.com>
2022-10-15 04:40:02 -0700

    fff

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("is positive\n");
+printf("n is positive\n");
 }
 else if (n == 0)
 {
-printf("is zero\n");
+printf("n is zero\n");
 }
 else
 {
-printf("is negative\n");
+printf("n is negative\n");
 }
 return (0);
 
