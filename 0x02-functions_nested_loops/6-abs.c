dbeb8f417a82fc9c7a6eecdbf0aa9374c52f5543
chuqxie <ekechukwudi63@gmail.com>
2022-10-18 18:00:42 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+
+* _abs - prints abs value
+* @n: char to print
+* Return: 0 on success
+*/
+
+int _abs(int n)
+
+{
+
+if (n > 0)
+
+{
+return (n);
+
+}
+
+else if (n == 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+
+}
+}
