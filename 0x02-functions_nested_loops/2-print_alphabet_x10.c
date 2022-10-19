4f79c2eede7b5923dc35f40fbd3e5f796891cf27
chuqxie <ekechukwudi63@gmail.com>
2022-10-18 17:33:46 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print alphabet to stdo
+* Return: Always 0
+*/
+
+void print_alphabet_x10(void)
+
+{
+
+int i, j;
+
+
+
+for (i = 0; i < 10; i++)
+
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+
+_putchar(j);
+
+}
+
+_putchar('\n');
+
+}
+
+}
