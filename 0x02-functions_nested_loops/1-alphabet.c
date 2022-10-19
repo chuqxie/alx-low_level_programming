83355b2d7db7ff5cb9cbf42e43247b3968710ece
chuqxie <ekechukwudi63@gmail.com>
2022-10-18 17:29:16 -0700

    vv

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet to stdo
+* Return: on success 1
+*/
+
+void print_alphabet(void)
+
+{
+
+int n;
+
+
+
+for (n = 'a'; n <= 'z'; n++)
+
+{
+
+_putchar(n);
+
+}
+
+_putchar('\n');
+
+}
