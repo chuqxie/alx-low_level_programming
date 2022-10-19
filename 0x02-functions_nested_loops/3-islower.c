ea38a2332d6873f3c819677912a98f4f3d157ce6
chuqxie <ekechukwudi63@gmail.com>
2022-10-18 17:41:17 -0700

    4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _islower - prints lower case alpha
+* @c: lowercase char to print
+* Return: Always 0
+*/
+
+int _islower(int c)
+
+{
+
+if (c >= 'a' && c <= 'z')
+
+{
+return (1);
+}
+
+else
+
+{
+
+return (0);
+}
+}
