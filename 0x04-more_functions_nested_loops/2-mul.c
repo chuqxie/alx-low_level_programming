58aa092e34e61b73d368f2ccd9c665e8679acf67
chuqxie <ekechukwudi63@gmail.com>
2022-10-20 05:55:36 -0700

    d

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* mul - multiply integers
+* @a: int to multiply
+* @b: int to multiply
+* Return: multiply
+*/
+
+
+
+int mul(int a, int b)
+
+{
+
+return (a * b);
+
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_line - print line
+* @n: integer to print
+* Return: void
+*/
+
+void print_line(int n)
+
+{
+
+int c = 0;
+
+
+
+while ((c < n) && (n > 0))
+
+{
+
+_putchar('_');
+
+c++;
+
+}
+
+_putchar('\n');
+}
